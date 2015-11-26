3d9eaeadf35a558bea85d2d10bc69d65fa1622ec
JR.Liu <liujianran@126.com>
2015-11-26 14:31:48 +0800

    加入两轴分开控制的打球的程序
    y轴的运动目前不受控制
    x轴的运动是一次运动
    zsp需要依赖老电脑,注意关闭防火墙。

---
+++ TabletennisCln/MainWindow.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Diagnostics;
+using System.Threading;
+
+using System.Windows.Threading;
+
+namespace TabletennisCln
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Thread RcvThread;
+        public MainWindow()
+        {
+            InitializeComponent();
+            vision = new cVision();
+            ZSPCt.BTC_WaitZSP(this, null);//自动打开端口，等待ZSP连接
+        }
+        //=======================================
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (RcvThread != null && RcvThread.IsAlive)
+            {
+                if (vision != null) { vision.newsock.Close(); }
+                RcvThread.Abort();
+            }    
+   
+
+                ZSPCt.UserControl_Unloaded(this, null);
+                Thread.Sleep(100);
+
+        }
+
+        #region 视觉部分代码
+        private readonly cVision vision; //接收视觉结果
+
+        public void ConectToVision()
+        {
+            string notes;
+            vision.initRcv();
+            RcvThread = new Thread(new ThreadStart(delegate
+            {
+                while (true)
+                {
+                    vision.ReciveData();
+                    notes = " X  = " + vision.hitPar[1].ToString() +
+                                   "\n Y  = " + vision.hitPar[2].ToString() +
+                                   "\n Z  = " + vision.hitPar[3].ToString() +
+                                   "\n Vx = " + vision.hitPar[4].ToString() +
+                                   "\n Vy = " + vision.hitPar[5].ToString() +
+                                   "\n Vz = " + vision.hitPar[6].ToString() +
+                                   "\n tg = " + vision.hitPar[12].ToString();
+                    if (vision.hitPar[0] == 1)
+                    {
+                        double[] padPar;
+                        SlovePadPram(vision.hitPar, out padPar);
+                        notes += "\n PadV = " + padPar[3].ToString() +
+                                 "\n PadS = " + padPar[4].ToString();
+                        SetNotes(notes);
+
+
+                        Control_Arm.Recv4Vision(vision.hitPar);
+                        ZSPCt.SetZSP((int)vision.hitPar[3],(int)(padPar[4]),127);
+                    }
+                    Thread.Sleep(50);
+                }
+            }
+            ));
+            RcvThread.Start();
+
+        }
+   
+        //------------------------------
+        private void SetNotes(string notes)
+        {
+            if (Dispatcher.Thread != Thread.CurrentThread)
+            {
+                this.TBK_VisionOut.Dispatcher.Invoke(new Action(() =>
+                {
+                    this.TBK_VisionOut.Text = notes;
+                }));
+            }
+            else
+            {
+                this.TBK_VisionOut.Text = notes;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ConectToVision();
+        }
+        #endregion
+
+        
+        #region 回球决策代码
+        public static double lambda = 0.9;
+        public static double length = 0.5;
+
+        private static bool SlovePadPram(double[] inputBall, out double[] output)
+        {
+            
+            output = new double[6];
+
+            double padX = inputBall[1];
+            double padY = inputBall[2];
+            double padZ = inputBall[3]/1000; 
+            double ballVx_In = inputBall[4] / 1000;
+            double ballVy_In = inputBall[5] / 1000;
+            double ballVz_In = inputBall[6] / 1000;
+
+            double g = 9.802;
+
+            //解方程 -1/2 * g * t^2 + Vz * t = -padZ
+            double a = -0.5 * g; double b = lambda*ballVz_In; double c = padZ;
+            double delta = b * b - 4 * a * c;
+
+            if (delta < 0) return false;//解算失败
+
+            double t1 = (-b - Math.Sqrt(delta)) / (2 * a);
+            double t2 = (-b + Math.Sqrt(delta)) / (2 * a);
+
+            double t = (t1 > t2) ? t1 : t2;
+
+            double ballV_Out = length / t;
+
+            double hitV = ballV_Out/lambda;
+
+            double padVy = Math.Sqrt((hitV * hitV) - (ballVx_In * ballVx_In)) + ballVy_In;
+
+            double padS = -Math.Acos(ballVx_In / hitV)/Math.PI * 180 - 90 + 127;
+
+            output[0] = padX;
+            output[1] = padY;
+            output[2] = padZ;
+            output[3] = padVy; //回球速度
+            output[4] = padS;  //回球球拍偏转方向
+            output[5] = 0;
+
+            Console.WriteLine("t = "+t.ToString() );
+            return true;
+        }
+        #endregion
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double length_tmp = Convert.ToDouble(TB_lambda.Text);
+            if (length_tmp == 0)
+            {
+                Console.WriteLine("Set length wrong");
+                return;
+            }
+            length = length_tmp;
+           
+        }
+
+    }
+}
---
+++ TabletennisCln/ZSPCt.xaml.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.ComponentModel;
+using System.Threading;
+
+namespace TabletennisCln
+{
+    /// <summary>
+    /// Interaction logic for ZSPCt.xaml
+    /// </summary>
+    public partial class ZSPCt : UserControl
+    {
+        public ZSPCt()
+        {
+            InitializeComponent();
+
+            d.ZSP_Statue = "Push to Connect";
+        }
+
+
+
+
+        public void BTC_WaitZSP(object sender, RoutedEventArgs e)
+        {
+            d.ZSP_Statue = "Wait for Connect";
+            d.statueThread.Start(); //检测按键变化
+            d.ZSP_Sender.StartWaitting();
+        }
+
+
+        public void BTC_HomeZSP(object sender, RoutedEventArgs e)
+        {
+            d.SendZSP_HomeCmd();
+        }
+
+        public void BTC_SevZ(object sender, RoutedEventArgs e)
+        {
+            d.ZSP_Sender.SendMsg("ServoZ");// Z轴开/关伺服命令
+        }
+
+        public void BTC_CloseZSP(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("here");
+        
+            d.ZSP_Sender.SendMsg("CloseZSP");// 关闭远端程序命令
+        }
+
+        public void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            BTC_CloseZSP(this, null);
+
+            d.statueThread.Abort();
+
+            d.ZSP_Sender.CloseSocket();
+
+        }
+
+
+        #region 外部接口
+        public void SetZSP(int z, int s, int p)
+        {
+            d.Hit_Z = z;
+            d.Hit_S = s;
+            d.Hit_P = p;
+        }
+        #endregion
+
+    }
+
+    public class data  : INotifyPropertyChanged//实现接口，详细表述MSDN
+    {
+        //INotifyPropertyChanged Members 不需更改---------------------------------------------------------
+        #region INotifyPropertyChanged Members
+
+        /// <summary>
+        /// Event to which the view's controls will subscribe.
+        /// This will enable them to refresh themselves when the binded property changes provided you fire this event.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// When property is changed call this method to fire the PropertyChanged Event
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged(string propertyName)
+        {
+            //Fire the PropertyChanged event in case somebody subscribed to it
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public Thread statueThread;
+
+        public data()
+        {
+            ZSP_Sender = new cTCPListenor();
+            Hit_Z = 300;
+            Hit_S = 127;
+            Hit_P = 127;
+
+            statueThread = new Thread(new ThreadStart(ThreadWork));
+            
+
+        }
+
+        public void ThreadWork()
+        {
+            while(ZSP_Sender!=null)
+            {
+                if (ZSP_Sender.isConected)
+                {
+                    ZSP_Statue = "Connected";
+                }
+                else
+                {
+                    ZSP_Statue = "Wait for Connect";
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+
+        #region MAX-MIN
+        public  int Z_MAX 
+        {
+            get { return 425; }
+            set {}
+        }
+
+        public  int Z_MIN
+        {
+            get { return 245; }
+            set { }
+        }
+
+        public  int S_MAX 
+        {
+            get { return 195; }
+            set {}
+        }
+        public int S_MIN
+        {
+            get { return 85; }
+            set { }
+        }
+
+        public  int P_MAX
+        {
+            get { return 195; }
+            set { }
+        }
+        public int P_MIN
+        {
+            get { return 65; }
+            set { }
+        }
+        #endregion
+
+        public cTCPListenor ZSP_Sender;
+
+        private int hit_Z;
+        public int Hit_Z
+        {
+            get { return hit_Z; }
+            set
+            {
+                hit_Z = value;
+                this.OnPropertyChanged("Hit_Z");
+                SendZSP_PosCmd();
+            }
+        }
+        private int hit_S;
+        public int Hit_S
+        {
+            get { return hit_S; }
+            set
+            {
+                hit_S = value;
+                OnPropertyChanged("Hit_S");
+                SendZSP_PosCmd();
+            }
+        }
+        private int hit_P;
+        public int Hit_P
+        {
+            get { return hit_P; }
+            set
+            {
+                hit_P = value;
+                OnPropertyChanged("Hit_P");
+                SendZSP_PosCmd();
+            }
+        }
+
+        private string zsp_statue;
+        public string ZSP_Statue
+        {
+            get { return zsp_statue; }
+            set
+            {
+                zsp_statue = value;
+                OnPropertyChanged("ZSP_Statue");
+            }
+        }
+
+
+
+
+        public void SendZSP_PosCmd()
+        {
+            //if (Hit_Z > Z_MAX || Hit_Z < Z_MIN
+            //|| Hit_S > S_MAX || Hit_S < S_MIN
+            //|| Hit_P > P_MAX || Hit_P < P_MIN)
+            //{
+            //    Console.WriteLine("ZSP 超限");
+            //    return;
+            //}
+            if (Hit_Z > Z_MAX) { Hit_Z = Z_MAX; }
+            if (Hit_Z < Z_MIN) { Hit_Z = Z_MIN; }
+            if (Hit_S > S_MAX) { Hit_S = S_MAX; }
+            if (Hit_S < S_MIN) { Hit_S = S_MIN; }
+            if (Hit_P > P_MAX) { Hit_P = P_MAX; }
+            if (Hit_P < P_MIN) { Hit_P = P_MAX; }
+
+            ZSP_Sender.SendMsg("z" + (hit_Z - Z_MIN).ToString() + "s" + hit_S.ToString() + "p" + hit_P.ToString() + "E");//这里- Constants.ZMin是为了满足UCHAR的类型范围。
+
+        }
+       
+        public void SendZSP_HomeCmd()
+        {
+            ZSP_Sender.SendMsg("ZHomeE");//Z轴复位命令
+            Hit_Z = 300;
+            Hit_S = 127;
+            Hit_P = 127;
+        }
+    }
+}
